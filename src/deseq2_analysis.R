695b9daec65790a6c04c26c65aa2af451aaea84b
Gitika (Gita) Bhardwaj <gita.bhardwaj1@gmail.com>
2026-09-07 18:03:55 -0500

    Update deseq2_analysis.R

--- src/deseq2_analysis.R
+++ src/deseq2_analysis.R
@@ -1,1 +1,13 @@
+library(DESeq2)
 
+counts_file <- "data/raw/GSE153740_raw_counts_GRCh38.p13_NCBI.tsv.gz"
+
+counts_df <- read.delim(
+  counts_file,
+  header = TRUE,
+  row.names = 1,
+  check.names = FALSE
+)
+
+dim(counts_df)
+head(counts_df)

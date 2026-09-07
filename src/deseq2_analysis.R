382e34b385d7b1c28f5adbf07c69061b067020bd
Gitika (Gita) Bhardwaj <gita.bhardwaj1@gmail.com>
2026-09-07 18:08:06 -0500

    Update deseq2_analysis.R

--- src/deseq2_analysis.R
+++ src/deseq2_analysis.R
@@ -11,3 +11,30 @@ counts_df <- read.delim(
 
 dim(counts_df)
 head(counts_df)
+sample_metadata <- data.frame(
+  row.names = colnames(counts_df),
+  sample = c("En1", "En2", "En3", "En6", "F10", "F14", "F15", "F9"),
+  group = factor(
+    c(
+      "Endometriosis",
+      "Endometriosis",
+      "Endometriosis",
+      "Endometriosis",
+      "Control",
+      "Control",
+      "Control",
+      "Control"
+    ),
+    levels = c("Control", "Endometriosis")
+  )
+)
+
+sample_metadata
+
+dds <- DESeqDataSetFromMatrix(
+  countData = counts_df,
+  colData = sample_metadata,
+  design = ~ group
+)
+
+dds
